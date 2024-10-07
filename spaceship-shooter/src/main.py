b8884bc99332c5dd0df89dd597c946c5a425930a
kiyomin0615 <moeday1010@gmail.com>
2024-10-07 11:19:33 +0900

    setup pygame

---
+++ spaceship-shooter/src/main.py
@@ -0,0 +1,19 @@
+import pygame
+
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Spaceship Shooter')
+is_running = True
+
+while is_running:
+  # event loop
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      is_running = False
+  
+  # render the game
+  display_surface.fill('blue')
+  pygame.display.update()
+
+pygame.quit()
